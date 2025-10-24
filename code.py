19b198a4bfae305430a5c34d85cf8ce8814eb1a3
Dana <djlarson3@gmail.com>
2025-10-23 21:32:06 -0500

    Make fade down smoother

--- code.py
+++ code.py
@@ -8,11 +8,9 @@ import microcontroller
 from fade_controller import Fade_Controller
 from press_observer import Press_Duration_Observer
 
-
 # === SETUP CONSTANTS ===
 SLEEP_TIME = os.getenv("SLEEP_TIME")
-ALPHA = 0.5 # FSR signal smoothing factor
-
+ALPHA = 0.5  # FSR signal smoothing factor
 
 # === SETUP NEOPIXEL ===
 PIXEL_ORDER = getattr(neopixel, os.getenv("PIXEL_ORDER"))
@@ -30,8 +28,10 @@ fsr = analogio.AnalogIn(getattr(board, os.getenv("FSR_PIN")))
 
 # === SETUP FADER AND PRESS OBSERVER ===
 COLOR = tuple(int(i.strip()) for i in os.getenv("COLOR")[1:-1].split(','))
-fader = Fade_Controller(pixels, COLOR)
-press_observer = Press_Duration_Observer(float(os.getenv("PRESS_DURATION_THRESHOLD")), 63000)
+
+PRESS_DURATION_THRESHOLD = float(os.getenv("PRESS_DURATION_THRESHOLD"))
+fader = Fade_Controller(pixels, COLOR, duration=PRESS_DURATION_THRESHOLD*700)
+press_observer = Press_Duration_Observer(PRESS_DURATION_THRESHOLD, 63000)
 
 # === FADE UP NEOPIXELS === 
 while not fader.is_finished():
@@ -42,23 +42,18 @@ while not fader.is_finished():
 filtered_value = fsr.value
 while not press_observer.duration_passed():
     filtered_value = ALPHA * fsr.value + (1-ALPHA) * filtered_value
+    #print((filtered_value,))
     press_observer.update(filtered_value)
     if press_observer.press_started:
-        fader.fade_down(0.2)
+        fader.fade_down()
     else:
         fader.fade_up()
     time.sleep(.01)
-
-# === FADE DOWN NEOPIXELS ===
-while not fader.is_finished():
-    fader.fade_down()
-    time.sleep(0.01)
 
 # === ENTER SLEEP ===
 print(f"Going to sleep for {SLEEP_TIME} seconds...")
 time_alarm = alarm.time.TimeAlarm(monotonic_time=time.monotonic() + SLEEP_TIME)
 alarm.exit_and_deep_sleep_until_alarms(time_alarm)
-
 
 # === Hard reset on wake-up ===
 # This line will only execute if somehow deep sleep fails
